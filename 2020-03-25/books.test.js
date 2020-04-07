d4fcd42fe78b512ffbbc49772eba6f9c0abf2ca1
Joe Fenton <joe.fenton@coopdigital.co.uk>
2020-04-07 13:28:33 +0100

    added new test for 3 different books

--- 2020-03-25/books.test.js
+++ 2020-03-25/books.test.js
@@ -1,45 +1,59 @@
-const VOL1 = "Vol1"
-const VOL2 = "Vol2"
-
-const DISCOUNT_PERCENTAGE = 0.95
-const PRICE = 8
-
-const bookCost = (number, discount=1) => number * PRICE * discount
-
-function calculateCost (...books){
-    const numberOfBooks = books.length
-    const numberOfDifferentBooks = new Set(books).size
-    const booksAllTheSame = numberOfDifferentBooks == 1
-
-    let cost 
-    if (numberOfBooks > 1 && !booksAllTheSame){
-        const duplicateBooks = numberOfBooks - numberOfDifferentBooks
-        const discountedPrice = PRICE * DISCOUNT_PERCENTAGE
-        cost = bookCost(numberOfDifferentBooks, DISCOUNT_PERCENTAGE) + bookCost(duplicateBooks)
-    } else {
-        cost = bookCost(numberOfBooks)
-    }
-    return cost
+const VOL1 = "Vol1";
+const VOL2 = "Vol2";
+const VOL3 = "Vol3";
+const discounts = {
+    1 : 1.0,
+    2 : 0.95,
+    3 : 0.9
 }
 
-describe("Buying books", () =>{
-    it("should cost 8 euros for 1 book", () => {
-        const cost = calculateCost(VOL1)
-        expect(cost).toBe(8)
-    })
+const discountPercentage = (numberOfBooks) => {
+    return discounts[numberOfBooks]
+}
+const PRICE = 8;
 
-    it("should cost 16 for two of the same books", () => {
-        const cost = calculateCost(VOL1, VOL1)
-        expect(cost).toBe(16)
-    })
+const bookCost = (number, discount = 1) => number * PRICE * discount;
 
-    it("should cost 15.20 for two different books", () => {
-        const cost = calculateCost(VOL1, VOL2)
-        expect(cost).toBe(15.20)
-    })
+function calculateCost(...books) {
+  const numberOfBooks = books.length;
+  const numberOfDifferentBooks = new Set(books).size;
+  const booksAllTheSame = numberOfDifferentBooks == 1;
 
-    it("should cost 8 + 15.20 for two different books and 1 duplicate", () => {
-        const cost = calculateCost(VOL1, VOL1, VOL2)
-        expect(cost).toBe(15.20 + 8)
-    })
-})
+  let cost;
+  if (numberOfBooks > 1 && !booksAllTheSame) {
+    const duplicateBooks = numberOfBooks - numberOfDifferentBooks;
+    cost =  bookCost(numberOfDifferentBooks, discountPercentage(numberOfDifferentBooks)) + bookCost(duplicateBooks);
+} else if (numberOfDifferentBooks === 3) {
+  cost = bookCost(3, 0.9);
+} else {
+    cost = bookCost(numberOfBooks);
+  }
+  return cost;
+}
+
+describe("Buying books", () => {
+  it("should cost 8 euros for 1 book", () => {
+    const cost = calculateCost(VOL1);
+    expect(cost).toBe(8);
+  });
+
+  it("should cost 16 for two of the same books", () => {
+    const cost = calculateCost(VOL1, VOL1);
+    expect(cost).toBe(16);
+  });
+
+  it("should cost 15.20 for two different books", () => {
+    const cost = calculateCost(VOL1, VOL2);
+    expect(cost).toBe(15.2);
+  });
+
+  it("should cost 8 + 15.20 for two different books and 1 duplicate", () => {
+    const cost = calculateCost(VOL1, VOL1, VOL2);
+    expect(cost).toBe(15.2 + 8);
+  });
+
+  it("should cost 21.60 for three different books", () => {
+    const cost = calculateCost(VOL1, VOL2, VOL3);
+    expect(cost).toBe(21.6);
+  });
+});
