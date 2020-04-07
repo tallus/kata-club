914d6718704256921c246b4fd1caaf00e3a781ad
Joe Fenton <joe.fenton@coopdigital.co.uk>
2020-04-07 13:54:59 +0100

    added final few tests to prove logic was sound for all combinations of purchase

--- 2020-03-25/books.test.js
+++ 2020-03-25/books.test.js
@@ -1,15 +1,18 @@
 const VOL1 = "Vol1";
 const VOL2 = "Vol2";
 const VOL3 = "Vol3";
+const VOL4 = "Vol4";
+const VOL5 = "Vol5";
 const discounts = {
     1 : 1.0,
     2 : 0.95,
-    3 : 0.9
+    3 : 0.9,
+    4 : 0.8,
+    5 : 0.75
 }
 
-const discountPercentage = (numberOfBooks) => {
-    return discounts[numberOfBooks]
-}
+const discountPercentage = (numberOfBooks) => discounts[numberOfBooks]
+
 const PRICE = 8;
 
 const bookCost = (number, discount = 1) => number * PRICE * discount;
@@ -23,6 +26,12 @@ function calculateCost(...books) {
 }
 
 describe("Buying books", () => {
+
+  it("should cost 0 euros for 0 book", () => {
+    const cost = calculateCost();
+    expect(cost).toBe(0);
+  });
+
   it("should cost 8 euros for 1 book", () => {
     const cost = calculateCost(VOL1);
     expect(cost).toBe(8);
@@ -51,5 +60,20 @@ describe("Buying books", () => {
   it("should cost 21.60 + 16.0 for three different books and two duplicates", () => {
     const cost = calculateCost(VOL1, VOL2, VOL2, VOL3, VOL3);
     expect(cost).toBe(21.6+16.0);
+  });
+
+  it("should cost 25.60 for 4 different books", () => {
+    const cost = calculateCost(VOL1, VOL2, VOL3, VOL4);
+    expect(cost).toBe(25.6);
+  });
+
+  it("should cost 30 for 5 different books", () => {
+    const cost = calculateCost(VOL1, VOL2, VOL3, VOL4, VOL5);
+    expect(cost).toBe(30);
+  });
+
+  it("should cost 30 + 16.0 for 5 different books", () => {
+    const cost = calculateCost(VOL1, VOL2, VOL3, VOL4, VOL5, VOL2, VOL4);
+    expect(cost).toBe(46);
   });
 });
