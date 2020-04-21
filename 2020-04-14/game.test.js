79c468eea33738ec5a0a8d3886864a533ff1aa0c
James Jeffries <james.jeffries@coopdigital.co.uk>
2020-04-21 13:59:58 +0100

    WIP on exploding musical chairs kata

--- 2020-04-14/game.test.js
+++ 2020-04-14/game.test.js
@@ -1,46 +1,62 @@
+class Player {
+    constructor(player, team){
+        this.player = player
+        this.team = team
+    }
+    passDynamite(team1, team2) {
+        if(this.team == 1) {
+            return team2[0]
+        } else {
+            return team1[1]
+        }
+    }
+}
 class Game {
-    constructor(){
-        this.holder = 1
+    constructor(team1, team2, rounds){
+        this.team1 = team1.map((player)=>new Player(player,1))
+        this.team2 = team2.map((player)=>new Player(player,2))
+        this.holder = this.team1[0]
+        this.rounds = rounds
     }
 
     winner(){
-        if (this.holder === 1){
+        if (this.holder.team == 1){
             return 2
-        } else if(this.holder === 2){
+        } else if(this.holder.team == 2){
             return 1
         }
     }
 
     round(){
-        if (this.holder === 1){
-            this.holder = 2
-        } else if(this.holder === 2){
-            this.holder = 1
+        this.holder = this.holder.passDynamite(this.team1, this.team2)
+    }
+
+    start() {
+        for(let i =0; i<this.rounds; i++) {
+            this.round()
         }
     }
 }
 
 test("2 players, 3 rounds, team 2 wins", () => {
-    const game = new Game(['a'], ['b'])
-    game.round()
-    game.round()
-    game.round()
+    const maxRounds = 3
+    const game = new Game(['a'], ['b'], maxRounds)
+    game.start()
     expect(game.winner()).toBe(1)
 })
 
 test('2 players, 4 rounds, team 1 wins', ()=>{
-    const game = new Game(['a'], ['b'])
-    game.round()
-    game.round()
-    game.round()
-    game.round()
+    const maxRounds = 4
+    const game = new Game(['a'], ['b'], maxRounds)
+    game.start()
     expect(game.winner()).toBe(2)
 })
 
 test('4 players, 3 rounds, team 2 wins', () => {
-    const game = new Game(['a', 'b'], ['c', 'd']) // TODO use these in Game
-    game.round('b')
-    game.round('a')
-    game.round('c')
+    const team1 = ['a', 'b']
+    const team2 = ['c', 'd']
+    const maxRounds = 3
+    const game = new Game(team1, team2, maxRounds)
+    game.start()
     expect(game.winner()).toBe(1)
 })
