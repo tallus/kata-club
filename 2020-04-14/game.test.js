5e100515757a779b46ddd83aadfd60e804b33f1a
Paul Munday <paul.munday@coop.co.uk>
2020-05-05 14:08:07 +0100

    adds changes from this weeks kata club

--- 2020-04-14/game.test.js
+++ 2020-04-14/game.test.js
@@ -3,17 +3,36 @@ class Player {
         this.player = player
         this.team = team
     }
+    nextPlayer(team){
+        console.log(this)
+        let index = team.indexOf(this)
+        console.log(team)
+        console.log(index)
+        return team.indexOf(this.player) + 1;
+    }
+
     passDynamite(team1, team2) {
         if(this.team == 1) {
-            return team2[0]
+            return team2[this.nextPlayer(team2)]
         } else {
-            return team1[0]
+            return team1[this.nextPlayer(team1)]
         }
     }
 }
 
-const teamOne = [new Player('A',1)]
-const teamTwo = [new Player('C', 2)]
+class SameTeamPassingPlayer extends Player {
+    passDynamite(team1, team2){
+        let team
+        if(this.team == 1){
+            team = team1[1]
+        } else {
+            team = team2[1]
+        }
+        return team
+    }
+}
+
+
 
 class Game {
     constructor(team1, team2, rounds){
@@ -38,6 +57,9 @@ class Game {
         }
     }
 }
+
+const teamOne = [new Player('A',1)]
+const teamTwo = [new Player('C', 2)]
 
 test("2 players, 3 rounds, team 2 wins", () => {
     const maxRounds = 3
@@ -69,4 +91,22 @@ test('4 players, 1 round, player 1 passes to same team', () => {
     const game = new Game(team1, team2, maxRounds)
     game.start()
     expect(game.winner()).toBe(2)
+})
+
+test('4 players, 2 rounds, player 1 passes to same team, different player', () => {
+    const team1 = [new SameTeamPassingPlayer('A', 1), new Player('B', 1)];
+    const team2 = [new Player('C', 2), new Player('D', 2)];
+    const maxRounds = 2;
+    const game = new Game(team1, team2, maxRounds);
+    game.start();
+    expect(game.winner()).toBe(1);
+})
+
+test.only('4 players, 2 rounds, player 2 passes to same team, different player', () => {
+    const team1 = [new Player('A', 1), new SameTeamPassingPlayer('B', 1)];
+    const team2 = [new Player('C', 2), new Player('D', 2)];
+    const maxRounds = 2;
+    const game = new Game(team1, team2, maxRounds);
+    game.start();
+    expect(game.holder).toBe(team1[1]);
 })
